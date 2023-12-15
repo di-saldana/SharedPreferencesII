3c8841f9a7303a2e31af5dcd6138d351f718c173
Dianelys Saldaña <dsl42@alu.ua.es>
2023-12-15 17:06:02 +0100

    Added Preference Screens

--- app/src/main/java/es/ua/eps/sharedpreferencesii/SettingsFragment.kt
+++ app/src/main/java/es/ua/eps/sharedpreferencesii/SettingsFragment.kt
@@ -2,9 +2,7 @@ package es.ua.eps.sharedpreferencesii
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,7 +14,8 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingsFragment : Fragment() {
+
+class SettingsFragment : PreferenceFragmentCompat() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -29,12 +28,8 @@ class SettingsFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
 
     companion object {
