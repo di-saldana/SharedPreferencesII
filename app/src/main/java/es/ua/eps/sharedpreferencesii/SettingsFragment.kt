5e3fea7964b44ef2867936ce7d90efad77ac9b4f
di-saldana <dsl42@alu.ua.es>
2023-12-18 14:30:50 +0100

    Added Summaries

--- app/src/main/java/es/ua/eps/sharedpreferencesii/SettingsFragment.kt
+++ app/src/main/java/es/ua/eps/sharedpreferencesii/SettingsFragment.kt
@@ -1,9 +1,12 @@
 package es.ua.eps.sharedpreferencesii
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceScreen
+import androidx.preference.SeekBarPreference
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,6 +39,21 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onNavigateToScreen(preferenceScreen: PreferenceScreen) {
         super.onNavigateToScreen(preferenceScreen)
         setPreferenceScreen(preferenceScreen)
+        updateSummaries()
+    }
+
+    private fun updateSummaries() {
+        val sizePreference = findPreference<ListPreference>("size")
+        val textColorPreference = findPreference<ListPreference>("text_color")
+        val backgroundColorPreference = findPreference<ListPreference>("background_color")
+        val alphaPreference = findPreference<SeekBarPreference>("alpha")
+        val rotationPreference = findPreference<SeekBarPreference>("rotation")
+
+        sizePreference?.summary = sizePreference?.value
+        textColorPreference?.summary = textColorPreference?.entry
+        backgroundColorPreference?.summary = backgroundColorPreference?.entry
+        alphaPreference?.summary = (alphaPreference?.value?.div(100.00)).toString()
+        rotationPreference?.summary = rotationPreference?.value.toString()
     }
 
     companion object {
